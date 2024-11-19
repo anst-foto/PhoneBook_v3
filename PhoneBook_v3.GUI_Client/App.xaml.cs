3c21a56981550229666e225bf87c1b31ac3c1e2e
Starinin Andrey <starinin-andrey@ya.ru>
2024-11-19 20:38:55 +0300

    Инициализация приложения

---
+++ PhoneBook_v3.GUI_Client/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PhoneBook_v3.GUI_Client;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
